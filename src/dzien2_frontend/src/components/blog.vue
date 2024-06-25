600110f9d2e2bef74c108f8607ac7cb4bde5427f
Kamil6065 <kamil06duda@gmail.com>
2024-06-25 19:55:20 +0200

    added a way to add new wpis

--- src/dzien2_frontend/src/components/blog.vue
+++ src/dzien2_frontend/src/components/blog.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
+        <button>@Click="pobierzWpisy"refresh</button>
         Siema blog!
+        {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
     </div>
 </template>
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+export default {
+    data() {
+        return {
+            wpisy: []
+            nowyBlog: ""
+        }
+    },
+    methods: {
+        async pobierzWpisy() {
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
+        }
+    },
+}
+</script>
