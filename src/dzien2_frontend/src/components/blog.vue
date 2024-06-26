84208db58223be37dd7e08bc0cf7f5a5df65fea0
Kamil6065 <kamil06duda@gmail.com>
2024-06-26 18:50:48 +0200

    edited blog.vue

--- src/dzien2_frontend/src/components/blog.vue
+++ src/dzien2_frontend/src/components/blog.vue
@@ -1,34 +1,37 @@
 <template>
     <div>
-        <h2 class="text-blue-600"Wpisy na bloga: </h2>
-            <div class="w-100 flex flex-row-reverse"></div>
-        <button@click="pobierzWpisy" class="float-right">refresh</button>
-        <div v-for="wpis is wpisy">
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy">refresh</button>
+        </div>
+        <div v-for="wpis in wpisy">
             <p>{{ wpis }}</p>
         </div>
         <input v-model="nowyBlog" type="text">
-        <button@click="dodajWpisy">dodaj</button>
+        <button @click="dodajWpisy">dodaj</button>
     </div>
 </template>
+
 <script>
 import { dzien2_backend } from 'declarations/dzien2_backend/index';
-export default {
-    data() {
-        return {
-            wpisy: []
+
+export default{
+    data(){
+        return{
+            wpisy:[],
             nowyBlog: ""
         }
     },
-    methods: {
-        async dodajWpisy() {
-            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+    methods:{
+        async dodajWpisy(){
+            this.wpisy = await dzien2_backend.dodaj_wpis(this.nowyBlog);
         },
-        async pobierzWpisy() {
+        async pobierzWpisy(){
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
-    async mounted() {
+    async mounted(){
         this.pobierzWpisy()
-    },
+    }
 }
 </script>
